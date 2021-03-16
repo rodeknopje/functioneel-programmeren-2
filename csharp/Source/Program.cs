260c9ce9a88e63257eb65465f78b12b60dfa2f92
rodeknopje <mn.stokhorst@gmail.com>
2021-03-16 21:05:28 +0100

    added csharp solution

---
+++ csharp/Source/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace csharp
+{
+    static class Program
+    {
+        static int[,] nodes;
+        
+
+        static void Main()
+        {
+
+            for (var y = 0; y < lines.Length; y++)
+            {
+                var line = "";
+                
+                for (var x = 0; x < lines.First().Length; x++)
+                {
+                    line += lines[y][x];
+                }
+
+                
+            }
+        }
+
+        static void CreateGrid()
+        {
+            var lines = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/../../../../input/1.txt");
+
+            nodes = new int[,1];
+
+        }
+        
+    }
+
+}
